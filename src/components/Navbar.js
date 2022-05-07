9f8c36887e9ac19c52b96719eeb17c20f2e6bc17
smstalker <sarah.stalker15@outlook.com>
2022-05-07 15:04:40 -0600

    moved files

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-header">React</h1>
+               
+                <ul className="list-inline">
+                    {MenuItems.map((item, index) =>{
+                        return(
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+            
+        )
+    }
+
+}
+
+
+export default Navbar
